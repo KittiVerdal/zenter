483c3f9d3848d639f89283f97f3237535934eef0
Kitti Verdal <kitti@jbl.no>
2019-10-15 17:40:33 +0200

    Nav skeleton finished (no styles

--- layouts/partials/nav.vue
+++ layouts/partials/nav.vue
@@ -1,17 +1,40 @@
 <template>
-  <header>
-    <nuxt-link to="/" class="logo">Zenter</nuxt-link>
-    <nav>
-      <ul>
-        <li><nuxt-link to="akupunktur"></nuxt-link>Om Akupunktur</li>
-        <li><nuxt-link to="plager"></nuxt-link>Plager</li>
-        <li><nuxt-link to="behandlinger"></nuxt-link>Behandlinger</li>
-        <li><nuxt-link to="kontakt"></nuxt-link>Kontakt</li>
-      </ul>
-    </nav>
-  </header>
+  <div>
+    <b-navbar toggleable="lg" class="[ navheader--background ]">
+      <b-navbar-bran class="[ navheader_logo ]">
+          <nuxt-link to="/">Zenter</nuxt-link>
+        </b-navbar-bran>
+      
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="[ navheader_list ]">
+          <b-nav-item class="[ navheader_listitem ]"
+            ><nuxt-link to="akupunktur">Om Akupunktur</nuxt-link></b-nav-item
+          >
+          <b-nav-item class="[ navheader_listitem ]"
+            ><nuxt-link to="plager">Plager</nuxt-link></b-nav-item
+          >
+          <b-nav-item class="[ navheader_listitem ]"
+            ><nuxt-link to="behandlinger">Behandlinger</nuxt-link></b-nav-item
+          >
+          <b-nav-item class="[ navheader_listitem ]">
+              <nuxt-link to="kontakt">Kontakt</nuxt-link></b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 <script>
 export default {}
 </script>
-<style scoped></style>
+
+<style lang="sass">
+.navheader{
+  &--background{
+    margin: 0;
+    padding:10px
+    background-color: rgba($secondaryDark, .6)
+  }
+}
+</style>
