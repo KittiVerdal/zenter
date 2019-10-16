bd250633c568389b9113730d9b9e3214ebb10336
Kitti Verdal <kitti@jbl.no>
2019-10-16 12:12:16 +0200

    commit for tutor help

--- plugins/slide-menu.js
+++ plugins/slide-menu.js
@@ -1,9 +1,16 @@
-import Vue from "vue"
-import { Slide, ScaleDown, ScaleRotate, Reveal, Push, PushRotate } from "vue-burger-menu"
+import Vue from 'vue'
+import {
+  Slide,
+  ScaleDown,
+  ScaleRotate,
+  Reveal,
+  Push,
+  PushRotate
+} from 'vue-burger-menu'
 
-Vue.component("Slide", Slide)
-Vue.component("ScaleDownMenu", ScaleDown)
-Vue.component("ScaleRotateMenu", ScaleRotate)
-Vue.component("RevealMenu", Reveal)
-Vue.component("PushMenu", Push)
-Vue.component("PushRotateMenu", PushRotate)
+Vue.component('Slide', Slide)
+Vue.component('ScaleDownMenu', ScaleDown)
+Vue.component('ScaleRotateMenu', ScaleRotate)
+Vue.component('RevealMenu', Reveal)
+Vue.component('PushMenu', Push)
+Vue.component('PushRotateMenu', PushRotate)
