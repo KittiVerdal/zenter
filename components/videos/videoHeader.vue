bb47c133c4aa8d9fdee8ad9cdc161fbb268ce981
Kitti Verdal <kitti@jbl.no>
2019-10-17 12:10:35 +0200

    Header with title skeleton - styled for desktop

--- components/videos/videoHeader.vue
+++ components/videos/videoHeader.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app" class="[ video ]">
-    <video ref="videoRef" src="~assets/images/Frontmovie-styles_2.mp4" id="video-container" width="100%" controls></video>
-    <b-img src="~/assets/images/circle270x270.svg" class="[ footer_logo  footer_logo--small ]" alt="Logo Akupunkturforeningen" title="Logo Akupunkturforeningen"></b-img>
-    <div class="[ header_container ]">
-      <h2 class="[ header_subHeader ]">Akupunktur</h2>
-      <h1 class="[ header_headline ]">Kvalitet i hver<br />time hver dag</h1>
-      <p class="[ header_text ]">Effekten av akupunktur er godt dokumentert for en rekke lidelser. Gi kroppen de rette verktøyene for å øke kvaliteten i din hverdag.</p>
+    <video ref="videoRef" src="~assets/images/Frontmovie-styles_2.mp4" id="video-container" width="100%" alt="Bilde av ung, sunn dame på fjellet" controls></video>
+    <b-img src="~/assets/images/circle270x270.svg" class="[ cicles ]" alt="Animerte sirkler"></b-img>
+    <div class="[ header header_container ]">
+      <h2 class="[ header header_subHeader ]">Akupunktur</h2>
+      <h1 class="[ header header_headline ]">Kvalitet i hver<br />time hver dag</h1>
+      <p class="[ header header_text ]">Effekten av akupunktur er godt dokumentert for en rekke lidelser. Gi kroppen de riktige verktøyene for å øke kvaliteten i din hverdag.</p>
       <primaryBtnOutline>Book online</primaryBtnOutline>
       <primaryBtnOutline>Ring oss</primaryBtnOutline>
     </div>
   </div>
 </template>
-
-
 <script>
 import primaryBtnOutline from '~/components/buttons/primaryBtnOutline'
 
@@ -32,4 +30,37 @@ export default {
     margin-top:-80px;
     z-index: 1;
     }
+
+.header{
+    font-family: $zenterfont;
+    letter-spacing: 0.8px;
+    color:$white;
+
+   &_container{
+       background-color: rgba($secondaryDark, .7);
+       width: 40%;
+       position:absolute;
+       top:200px;
+       right:50px;
+       padding:40px;
+   }
+   &_subHeader{
+       font-weight: 400;
+       text-transform: uppercase;
+       font-size: 1.5em;
+       color: $primary;
+   }
+   &_headline{
+       font-weight: 700;
+       font-size:4.8em;
+       line-height: 80px;
+   }
+   &_text{
+       font-size: 1.2em;
+       padding:20px 0px; 
+   }
+}
+.btnOutlined {
+    margin-right:20px;
+}
 </style>
