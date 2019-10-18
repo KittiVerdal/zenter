b3f1ca46d6430bed72707ce1528bc4b000049a14
Kitti Verdal <kitti@jbl.no>
2019-10-19 01:04:24 +0200

    TreatmentCard data added

---
+++ constants/treatmentCards.js
@@ -0,0 +1,74 @@
+export const articleCards = [
+  {
+    id: 1,
+    path: require('@/assets/images/akupunktur.jpg'),
+    category: 'Behandlinger',
+    title: 'Akupunktur',
+    alt: 'Svanhild M. Øvrebø setter akupunkturnåler på pasient',
+    titleattr: 'Svanhild M. Øvrebø setter akupunkturnåler på pasient',
+    link: '/behandlinger'
+  },
+  {
+    id: 2,
+    path: require('@/assets/images/kopping.jpg'),
+    category: 'Behandlinger',
+    title: 'Kopping',
+    alt: 'Svanhild M. Øvrebø setter kopper på pasient',
+    titleattr: 'Svanhild M. Øvrebø setter kopper på pasient',
+    link: '/behandlinger'
+  },
+  {
+    id: 3,
+    path: require('@/assets/images/kosmetisk-akuunktur.jpg'),
+    category: 'Behandlinger',
+    title: 'Kosmetisk Akupunktur',
+    alt: 'Svanhild M. Øvrebø setter akupunktur på pasient',
+    titleattr: 'Svanhild M. Øvrebø setter akupunktur på pasient',
+    link: '/behandlinger'
+  },
+  {
+    id: 4,
+    path: require('@/assets/images/moxa.jpg'),
+    category: 'Behandlinger',
+    title: 'Moxa',
+    alt: 'Moxa røkelse',
+    titleattr: 'Moxa røkelse',
+    link: '/behandlinger'
+  },
+  {
+    id: 5,
+    path: require('@/assets/images/øreakupunktur.jpg'),
+    category: 'Behandlinger',
+    title: 'Øreakupunktur',
+    alt: 'Dame som behandles med øreakupunktur',
+    titleattr: 'Dame som behandles med øreakupunktur',
+    link: '/behandlinger'
+  },
+  {
+    id: 6,
+    path: require('@/assets/images/card-needles.svg'),
+    category: 'Behandlinger',
+    title: 'Triggerpunkt og Ørelys',
+    alt: 'Illustrasjonsbildet med 2 akupunkturnåler',
+    titleattr: 'Illustrasjonsbildet med 2 akupunkturnåler',
+    link: '/behandlinger'
+  },
+  {
+    id: 7,
+    path: require('@/assets/images/elektro-akupunktur.jpg'),
+    category: 'Behandlinger',
+    title: 'Elektroakupunktur',
+    alt: 'Illustrasjonsbildet av elektroakupunktur',
+    titleattr: 'Illustrasjonsbildet av elektroakupunktur',
+    link: '/behandlinger'
+  },
+  {
+    id: 8,
+    path: require('@/assets/images/guasha.jpg'),
+    category: 'Behandlinger',
+    title: 'Guasha',
+    alt: 'Illustrasjonsbilde av Guasha-behandling',
+    titleattr: 'Illustrasjonsbilde av Guasha-behandling',
+    link: '/behandlinger'
+  }
+]
